3c3a649391bfe34bbc0538215d03392d163b6656
Asim M Al Twijry <a.altwijry@nasaq.tech>
2018-11-09 16:39:28 +0300

    small updates

--- app/js/inject.js
+++ app/js/inject.js
@@ -10,7 +10,7 @@ window.onload = function() {
          */
         function showImportButton() {
           // CSS to style our buttons
-          $('head').append("<style>.btn{color:#fff;display:inline-block;padding:6px 12px;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;ms-touch-action:manipulation;touch-action:manipulation;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;background-image:none;border:1px solid transparent;border-radius:4px;margin-bottom:10px}.importButton{background-color:#1E88E5}.importAllButton{background-color:#F44336}</style>");
+          $('head').append("<style>.btn{color:#fff;display:inline-block;padding:6px 12px;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;ms-touch-action:manipulation;touch-action:manipulation;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;background-image:none;border:1px solid transparent;border-radius:4px;margin-bottom:10px}.importButton{background-color:#1E88E5; max-width: 94px;float: right;}.importAllButton{background-color:#F44336}</style>");
 
           // Add import button to each subject
           $('.qv_table').prepend('<button class="btn importButton">Subject Found. Import Classes?</button>');
--- chrome/js/inject.js
+++ chrome/js/inject.js
@@ -5,21 +5,17 @@ showImportButton();
  * Shows buttons to allow the user to import subjects into the timetabler
  */
 function showImportButton() {
+
+  console.log("ASiiim");
+
   // Add import button to each subject
   var importButton = crel("button",
-    {"class": "btn importButton"},
-    "Subject Found. Import Classes?"
+    { "class": "btn importButton" },
+    "+"
   );
-  $(".qv_table").prepend(importButton);
+  $("#myForm\\:offeredCoursesTable.rowFlow tbody tr.ROW1").prepend(importButton);
+  $("#myForm\\:offeredCoursesTable.rowFlow tbody tr.ROW2").prepend(importButton);
 
-  // Add importAll button if more than one subject exists
-  if ($(".qv_table").length > 1) {
-    var importAllButton = crel("button",
-      {"class": "btn importAllButton"},
-      "Multiple Subjects Found. Import ALL Classes?"
-    );
-    $(".divider:nth-of-type(1)").prepend(importAllButton);
-  }
 }
 
 /**
@@ -27,16 +23,16 @@ function showImportButton() {
  */
 function exportUnitData(table) {
   // Get the unit ID and subject name from above the table
-  var unitString;
-  if (location.pathname == "/qv/ttab_student_p.show") { // Enrollment page
-    unitString = $(table).prev().prev().find("strong").html().trim();
-  } else { // Search page
-    unitString = $(table).prevAll("h2:first").html();
-  }
+  var td = $(table).find('td');
+
+
 
   // Extract the unit ID (e.g. MAB126) and name (e.g. Mathematics)
-  var unitID = unitString.split(" ")[0];
-  var unitName = unitString.substr(unitString.indexOf(" ") + 1);
+  var unitID = td.eq(0).text().trim()
+  console.log("unitID: " + unitID);
+
+  var unitName = td.eq(1).text().trim()
+  console.log("unitID: " + unitName);
 
   // Create a new array for the unit
   var unitData = {
@@ -46,24 +42,74 @@ function exportUnitData(table) {
   };
 
   // Extract all of the row data from the table
-  $(table).find("tr:not(:first-child)").each(function (index, element) {
-    var td = $(this).children("td");
-    var classData = {
-      "className": td.eq(0).text().trim(),
-      "classType": td.eq(1).text(),
-      "day": td.eq(2).text(),
-      "time":  { // raw = "11:00AM-01:00PM" or "11:00am - 01:00pm"
-        "raw" : td.eq(3).text().toLowerCase().replace("m-", "m - "),
-        "start" : td.eq(3).text().split("-")[0].trim(),
-        "end" : td.eq(3).text().split("-")[1].trim(),
-      },
-      "location": td.eq(4).text().trim(),
-      "staff": td.eq(5).text().replace(/(\r\n|\n|\r)/gm, "")
-    };
+  console.log("td.eq(6): ");
+  console.log(td.eq(6).children().text());
+  td.eq(6).each(function (i, v) {
+    console.log(i);
+    var details = $(v).children().eq(0).children();
 
-    // Push the row data into our classInfo object
-    unitData.classes.push(classData);
-  });
+    //console.log(" 2 @t 11:00 ص - 12:40 م @r 201 د (D) @n  3 @t 04:00 م - 04:50 م @r 204 أ (A)");
+    //console.log(" 5 @t 01:00 م - 02:40 م @r ");.
+    //console.log("01:00 م - 02:40 م @r ");
+    //  1 @t 01:00 م - 02:40 م @r 204 أ (A) @n  4 @t 03:00 م - 03:50 م @r 204 أ (A)
+
+    for (var i = 0; i < (details.eq(1).val().match(/@t/g) || []).length; i++) {
+      var detailsCopy = details.eq(1).val();
+      if (detailsCopy.indexOf('@n ') != -1) {
+        //there is more than one timeslot
+        var oneSlot = detailsCopy.substring(0, detailsCopy.indexOf('@n ') + 3)
+        detailsCopy = detailsCopy.substring(0, detailsCopy.indexOf('@n ') + 3)
+
+      } else {
+        var oneSlot = detailsCopy.substring(0, detailsCopy.indexOf('@t ') + 3)
+      }
+      console.log(JSON.stringify(oneSlot));
+
+
+      oneSlot = oneSlot.replace(/\s/g,'')
+      console.log(JSON.stringify(oneSlot));
+
+      oneSlot = oneSlot.replace("1@t", 'U')
+      oneSlot = oneSlot.replace("2@t", 'M')
+      oneSlot = oneSlot.replace("3@t", 'T')
+      oneSlot = oneSlot.replace("4@t", 'W')
+      oneSlot = oneSlot.replace("5@t", "R")
+      oneSlot = oneSlot.replace("6@t", 'F')
+      oneSlot = oneSlot.replace("7@t", 'S')
+      oneSlot = oneSlot.replace("ص", 'am')
+      oneSlot = oneSlot.replace("م", 'pm')
+
+      console.log("new String: " + oneSlot)
+      var day = oneSlot.slice(0, 1)
+      oneSlot = oneSlot.substring(1);
+      console.log("oneSlot: " + oneSlot);
+
+      var rawTime = oneSlot
+      console.log(oneSlot.replace("5 @t ", 'R'));
+
+
+      var classData = {
+        "className": td.eq(1).text().trim(),
+        "classType": td.eq(3).text(),
+        "day": day,
+        "time": { // raw = "11:00AM-01:00PM" or "11:00am - 01:00pm"
+          "raw": td.eq(3).text().toLowerCase().replace("m-", "m - "),
+          "start": td.eq(3).text().split("-")[0].trim(),
+          "end": td.eq(3).text().split("-")[1].trim(),
+        },
+        "location": td.eq(4).text().trim(),
+        "staff": td.eq(5).text().replace(/(\r\n|\n|\r)/gm, "")
+      };
+
+      // Push the row data into our classInfo object
+      unitData.classes.push(classData);
+
+    }
+
+
+
+  })
+
 
   sendUnit(unitData);
 }
@@ -81,17 +127,20 @@ function sendUnit(unitData) {
 /**
  * Add an event listener for the import button
  */
-$("button.importButton").bind("click", function() {
+$("button.importButton").css("position", "absolute");
+$("button.importButton").bind("click", function () {
   // Check the timetabler is running
   var sender = $(this);
 
-  chrome.runtime.sendMessage({type: "checkTab"}, function(response) {
+  chrome.runtime.sendMessage({ type: "checkTab" }, function (response) {
     if (response == "Done!") {
       // Set the button to show the class has been imported
       sender.css("background-color", "#4CAF50");
       sender.text("Imported!");
 
       // Import the classes into the timetabler
+      console.log(sender.parent());
+
       exportUnitData(sender.parent());
     }
   });
@@ -100,19 +149,19 @@ $("button.importButton").bind("click", function() {
 /*
  * Add an event listener for the import all button
  */
-$("button.importAllButton").bind("click", function() {
+$("button.importAllButton").bind("click", function () {
   // Check the timetabler is running
-  chrome.runtime.sendMessage({type: "checkTab"}, function(response) {
+  chrome.runtime.sendMessage({ type: "checkTab" }, function (response) {
     // Wait for the timetabler to say it has finished loading
     if (response == "Done!") {
       // Set the butten to show the class has been imported
-      $(".btn").each(function() {
+      $(".btn").each(function () {
         $(this).css("background-color", "#4CAF50");
         $(this).text("Imported!");
       });
 
       // Import the classes into the timetabler
-      $(".qv_table").each(function() {
+      $(".rowFlow").each(function () {
         exportUnitData(this);
       });
     }
